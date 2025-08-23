8fde52f6a7ef46d981662a1e1b7419c08e448411
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-08-23 19:15:23 +0330

    rhrhrh

--- Arduino/openmv/openmv.ino
+++ Arduino/openmv/openmv.ino
@@ -1,9 +1,12 @@
 #include <rcj_robot.h>
 RCJ_Robot r;
 int cnt_back = 0;
+int cnt_ball_in_kick = 0;
+int target;
+#define Look_At_Goal false
 void setup() {
   r.robot1_x = 158;
-  r.robot1_y = 120;
+  r.robot1_y = 90;
   r.GY_SENS = 80;
   r.init();
   r.buzzer(1);
@@ -14,7 +17,12 @@ void setup() {
 void loop() {
   r.readSensors();
   r.readInputs();
-  r.read_GY();
+  if (r.is_yellow && Look_At_Goal) {
+    target = r.yellow_angle;
+    if (target > 180) target -= 360;
+    r.GY = target * 3;
+  }
+  else r.read_GY();
   r.read_MV();
   if (r.DIP2) r.printSensors();
   else r.printCam();
@@ -22,21 +30,27 @@ void loop() {
   if (r.SW1 && r.Ball_In_Kicker) r.shoot();
   if (r.DIP2 && r.DIP1) r.spin(1);
   else if (r.DIP2) r.spin(0);
-
   if (r.DIP2) r.stop();
   else if (r.Ball_In_Kicker) {
     r.spin(r.DIP1);
-    if (r.is_yellow && r.DIP1) {
-      if (r.lookToward(r.yellow_angle)) {
-        r.stop();
-        r.shoot();
-      }
-    } else {
+    cnt_ball_in_kick++;
+    if (cnt_ball_in_kick > 20) {
+      r.moveAngle(180);
+      if (cnt_ball_in_kick > 40) cnt_ball_in_kick = 0;
+    } 
+    // else if (r.is_yellow) {
+    //   r.speed = 65000;
+    //   if (target < 10 && target > -10) {
+    //     r.shoot();
+    //   } else r.move(0);
+    // } 
+    else {
       r.speed = 65000;
       r.move(0);
       r.shoot();
     }
   } else if (r.is_ball) {
+    cnt_ball_in_kick = 0;
     cnt_back = 0;
     r.spin(r.DIP1);
     if (r.distance_ball < 95) {
@@ -44,7 +58,7 @@ void loop() {
       if (r.ball_angle < 15 || r.ball_angle > 345) {
         r.moveAngle(0);
         r.speed = 40000;
-      } else if (r.ball_angle < 40 || r.ball_angle > 320) {
+      } else if (r.ball_angle < 20 || r.ball_angle > 340) {
         r.moveAngle(r.ball_angle);
         r.speed = 30000;
       } else if (r.ball_angle < 60) r.moveAngle(r.ball_angle + 40);
@@ -60,7 +74,8 @@ void loop() {
       else r.moveAngle(r.ball_angle - 60);
     }
   } else {
-    if (cnt_back < 30) r.moveAngle(180);
+    r.speed = 30000;
+    if (cnt_back < 50) r.moveAngle(180);
     else r.stop();
     r.spin(0);
     cnt_back++;
